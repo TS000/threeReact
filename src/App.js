6717ebab4237f2a62764b96546b1e19b4c607221
TS000 <tylersnell@me.com>
2019-01-24 11:24:22 -0700

    added color changing

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import * as THREE from 'three';
+
 class ThreeScene extends Component{
+
   componentDidMount(){
     const width = this.mount.clientWidth
     const height = this.mount.clientHeight
+
     //ADD SCENE
     this.scene = new THREE.Scene()
+
     //ADD CAMERA
     this.camera = new THREE.PerspectiveCamera( 30, window.innerWidth/window.innerHeight, 0.1, 1000 )
     this.camera.position.z = 100
+
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setClearColor('papayawhip')
     this.renderer.setSize(width, height)
     this.mount.appendChild(this.renderer.domElement)
+
     //ADD CUBE
     const geometry = new THREE.BoxGeometry( 20, 20, 20)
 
@@ -21,9 +27,10 @@ class ThreeScene extends Component{
       geometry.faces[ d ].color.setHex( Math.random() * 0xffffff );
   }
 
-    const material = new THREE.MeshBasicMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } )
+    const material = new THREE.MeshNormalMaterial( { color: 0xffffff, vertexColors: THREE.FaceColors } )
     this.cube = new THREE.Mesh(geometry, material)
     this.scene.add(this.cube)
+
     //ADD LIGHT
     const light = new THREE.PointLight(0xFFF00)
     light.position.set(10,0, 25)
@@ -40,6 +47,7 @@ componentWillUnmount(){
     this.stop()
     this.mount.removeChild(this.renderer.domElement)
   }
+
 start = () => {
     if (!this.frameId) {
       this.frameId = requestAnimationFrame(this.animate)
@@ -65,7 +73,7 @@ render(){
           ref={(mount) => { this.mount = mount }}
         />
         <div style={{ position: 'absolute', margin: '0 auto', textAlign: 'center', top: '45%', right: '45%', color: 'MediumSlateBlue'}}>
-          <h1>THREEJS + REACT</h1>
+          <h1>THREEjs + REACT</h1>
         </div>
         <div style={{ textAlign: 'center'}}>
         <h4>*refresh to change the shape</h4>
