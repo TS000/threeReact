f63e6ed92c147899d12921075aa8c8172e644c7c
TS000 <tylersnell@me.com>
2019-03-20 10:54:15 -0600

    fixed about page padding

--- src/layouts/About.js
+++ src/layouts/About.js
@@ -7,7 +7,7 @@ const AboutWrap = Styled.div`
   justify-content: center;
   padding: 15% 10% 10% 10%;
   @media screen and (min-width: 1000px) {
-    padding: 20%;
+    padding: 10% 30%;
   }
     h1, p{
       color: white;
@@ -24,7 +24,9 @@ export default class About extends Component {
         <AboutWrap>
           <div>
           <h1>Outer space is the cosmic relative of a dream.</h1>
-          <p>Deep Sleep is a musical concept project from individuals who see sleep as an unexplored frontier. The music is based around the way an individual sleeps and the dreams or nightmares that find them when they rest. Subjects include a newly homeless youth staying at a shelter for the first time, a school teacher who suffers from night terrors, and an investment banker who has just given away all their assets in preparation for the rapture. This space will be updated with music in the near future. Sleep well. </p>
+          <p>Deep Sleep is a musical concept project from individuals who see sleep as an unexplored frontier. The music is based around the way an individual sleeps and the dreams or nightmares that find them when they rest.</p> 
+          <p>Subjects include a newly homeless youth staying at a shelter for the first time, a school teacher who suffers from night terrors, and an investment banker who has just given away all of their assets in preparation for the rapture.</p>
+          <p>This space will be updated with music in the near future. Sleep well. </p>
         </div>  
       </AboutWrap>
       </React.Fragment>
